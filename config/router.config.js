c38262f129533c675643eb024a8a0a265a01d0c7
一碟茴香豆 <zhaoke.deng.whrd@outlook.com>
2019-12-30 10:44:25 +0800

    删除用户界面

--- config/router.config.js
+++ config/router.config.js
@@ -1,13 +1,4 @@
 export default [
-  // user
-  {
-    path: '/user',
-    component: '../layouts/UserLayout',
-    routes: [
-      { path: '/user', redirect: '/user/login' },
-      { path: '/user/login', component: './User/Login' },
-    ],
-  },
   // H5
   {
     path: '/',
--- src/pages/User/Login.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-
-export default class extends React.PureComponent {
-  public render() {
-    return (<div>Login</div>);
-  }
-}
